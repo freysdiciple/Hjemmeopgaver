2b6bd0632da8901ad19ad4d86653973bfcb594f1
Massimo Hansen <massimo.hansen@gmail.com>
2020-10-04 17:59:07 +0200

    Update Opgave3.java

--- Opgave3.java
+++ Opgave3.java
@@ -1,32 +1,73 @@
 package Hjemmeopgave2;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Opgave3 {
 	
-	static double needleLength = 1;
-	static int nOfReps = 1000000;
+	static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
-		int nOfCrosses = train(nOfReps);
-		System.out.println((double) nOfReps/ (double) nOfCrosses);
+		//Gets user input
+		int nReps = getInput();
+			
+		//Saves the number of times the needle crosses a line in a variable
+		int nOfCrosses = needleThrows(nReps);
+			
+		//Prints out the estimated value of PI
+		System.out.println((double)nReps/nOfCrosses);
 		
-
 	}
 	
-	public static int train(int repetitions) {
+	public static int getInput() {
+		
+		int reps=0;
+		System.out.print("Enter number of Iterations: ");
+		
+		//Throws an error if a non-integer is recieved as input
+		try {
+			reps = scanner.nextInt();
+		}
+		catch(InputMismatchException ime) {
+			inputError();
+			reps = getInput();
+		}
+		
+		//Throws an error if the received integer is zero or negative
+		if(reps <= 0) {
+			inputError();
+			reps = getInput();
+		}
+		return reps;
+	}
+	
+	public static int needleThrows(int repetitions) {
 		int cases=0;
 		
 		for(int i=0; i<repetitions; i++) {
+			//Determines a new midpoint and angle for the needle for every throw
 			double needleMiddle = 2*Math.random();
 			double needleAngle = Math.PI*Math.random();
 			
-			double needleEnd1 = needleMiddle + Math.sin(needleAngle) * needleLength/2;
-			double needleEnd2 = needleMiddle - Math.sin(needleAngle) * needleLength/2;
+			//Calculates the ends of the needles
+			double needleEnd1 = needleMiddle + Math.sin(needleAngle)/2;
+			double needleEnd2 = needleMiddle - Math.sin(needleAngle)/2;
 			
+			//Checks if needle crossed a line
 			Boolean crossed = (needleEnd1 > 2 || needleEnd2 < 0) ? true : false;
-			if(crossed) {cases++;}
+			if(crossed){cases++;}
 		}
 		return cases;
 	}
-
+	
+	public static void inputError() {
+		//Error message
+		System.out.println("\n======================================");
+		System.out.println("ERROR: PLEASE ENTER A POSITIVE INTEGER");
+		System.out.println("======================================\n");
+		
+		//Clears the next line
+		scanner.nextLine();
+	}
 }
